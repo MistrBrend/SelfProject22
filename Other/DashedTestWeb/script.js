ddf9abb11fc3d46542214585f8dadcb2932ef7ea
Brendan Kersten <1192333@student.roc-nijmegen.nl>
2023-02-01 00:27:27 +0100

    menu modal done

--- Other/DashedTestWeb/script.js
+++ Other/DashedTestWeb/script.js
@@ -1,14 +1,14 @@
- const open = document.querySelector("hamburger-menu-open");
- const close = document.querySelector("hamburger-menu-close");
+ const open = document.querySelector(".hamburger-menu-open");
+ const close = document.querySelector(".hamburger-menu-close");
 
  const menuList = document.querySelector(".menu-wrapper").classList;
 
- open.addEventListener("click", () => {
-    menuList.toggle("opened");
-    menuList.toggle("closed");
+ open.addEventListener('click', () => {
+   menuList.toggle("opened");
+   menuList.remove("closed");
  })
 
  close.addEventListener("click", () => {
     menuList.toggle("closed");
-    menuList.toggle("opened");
+    menuList.remove("opened");
  })
