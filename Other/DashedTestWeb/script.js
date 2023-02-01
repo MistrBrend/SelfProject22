9550fd8e321f2bbcc8cc94b3599e614133d4559c
Brendan Kersten <1192333@student.roc-nijmegen.nl>
2023-02-02 00:21:02 +0100

    ..

--- Other/DashedTestWeb/script.js
+++ Other/DashedTestWeb/script.js
@@ -1,14 +1,17 @@
- const open = document.querySelector(".hamburger-menu-open");
- const close = document.querySelector(".hamburger-menu-close");
+const open = document.querySelector(".hamburger-menu-open");
+const close = document.querySelector(".hamburger-menu-close");
+const menuPage = document.querySelector(".menu-wrapper").classList;
 
- const menuList = document.querySelector(".menu-wrapper").classList;
+const menuListChilds = document.querySelector("ul li a").classList;
+console.log(menuListChilds);
 
- open.addEventListener('click', () => {
-   menuList.toggle("opened");
-   menuList.remove("closed");
- })
 
- close.addEventListener("click", () => {
-    menuList.toggle("closed");
-    menuList.remove("opened");
- })
+open.addEventListener('click', () => {
+  menuPage.toggle("opened");
+  menuPage.remove("closed");
+})
+
+close.addEventListener("click", () => {
+  menuPage.toggle("closed");
+  menuPage.remove("opened");
+})
