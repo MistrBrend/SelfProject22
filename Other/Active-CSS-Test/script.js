b753952b4fbdee94c8038d6eea1be04fe41eab21
Brendan Kersten <1192333@student.roc-nijmegen.nl>
2023-02-03 00:50:39 +0100

    some small changes

--- Other/Active-CSS-Test/script.js
+++ Other/Active-CSS-Test/script.js
@@ -1,3 +1,12 @@
-const links = document.querySelectorAll(".link");
-console.log();
+const border = document.querySelector(".all-links");
+const links = document.querySelectorAll(".links");
 
+console.log(links);
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        border.querySelector(".active").classList.remove("active");
+        link.classList.add("active");
+    });
+
+});
